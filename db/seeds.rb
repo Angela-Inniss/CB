29e04ab770e2a5c7a60f6cdf35690d1ac0a0795b
Angela Inniss <Angela.inniss@hotmail.co.uk>
2019-05-20 22:17:58 +0100

    Seeded 2 services

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts 'Creating services...'
+Service.create!({
+  name: "Stain",
+  description: "v stubborn"
+})
+Service.create!({
+  name: "Wood",
+  description: "shiney clean "
+})
+puts 'Finished!'
