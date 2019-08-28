894ed58e063a27993e893a9d199e384c423d1256
Angela Inniss <Angela.inniss@hotmail.co.uk>
2019-08-28 13:52:03 +0100

    added new image upload with active storage- pulled images through to show page and index page

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,10 +1,13 @@
 class Service < ApplicationRecord
   has_many_attached :images
+   scope :with_eager_loaded_images, -> { eager_load(images_attachments: :blob) }
   mount_uploader :photo, PhotoUploader
   validates :category, presence: true
+
   include PgSearch
   pg_search_scope :search_by_category, against: :category,
      using: {
      tsearch: { prefix: true } # <-- now `superman batm` will return something!
-   }
+  }
+
 end
